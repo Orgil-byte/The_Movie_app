34a11e21a5e1de17e60caec18d8cc554c56cf0d3
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-02-06 10:28:28 +0800

    A

--- src/app/_components/Movielist/MovieList.tsx
+++ src/app/_components/Movielist/MovieList.tsx
@@ -1,43 +1,13 @@
-"use client";
-
-import { useState } from "react";
-import MovieCategory from "./MovieCategory";
-import Movies from "./Movies";
-import { dataUpcomingList } from "@/app/_data/movie-list-data";
-import { dataPopularList } from "@/app/_data/movie-list-data";
-import { dataTopRatedList } from "@/app/_data/movie-list-data";
+import { UpComingMovies } from "./MoviesMaps";
+import { PopularMovies } from "./MoviesMaps";
+import { TopRatedMovies } from "./MoviesMaps";
 
 const MovieList = () => {
-  const [list, setList] = useState(dataUpcomingList);
-  const [popularList, setPopularList] = useState(dataPopularList);
-  const [topRateList, setTopRateList] = useState(dataTopRatedList);
-
   return (
     <div className="px-5 flex flex-col w-full min-w-93.75 gap-8 md:px-10 lg:px-20 lg:gap-13 max-w-360">
-      <div className="flex flex-col gap-8">
-        <MovieCategory categoryName="Upcoming" />
-        <div className="grid grid-cols-2 gap-3 sm:gap-4 sm:grid-cols-3 md:gap-5 md:grid-cols-4 lg:gap-6 lg:grid-cols-5">
-          {list.map(({ rate, img, id, title }) => (
-            <Movies key={id} img={img} title={title} rate={rate} />
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col gap-8">
-        <MovieCategory categoryName="Popular" />
-        <div className="grid grid-cols-2 gap-3 sm:gap-4 sm:grid-cols-3 md:gap-5 md:grid-cols-4 lg:gap-6 lg:grid-cols-5">
-          {popularList.map(({ rate, img, id, title }) => (
-            <Movies key={id} img={img} title={title} rate={rate} />
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col gap-8">
-        <MovieCategory categoryName="Top Rated" />
-        <div className="grid grid-cols-2 gap-3 sm:gap-4 sm:grid-cols-3 md:gap-5 md:grid-cols-4 lg:gap-6 lg:grid-cols-5">
-          {topRateList.map(({ rate, img, id, title }) => (
-            <Movies key={id} img={img} title={title} rate={rate} />
-          ))}
-        </div>
-      </div>
+      <UpComingMovies />
+      <PopularMovies />
+      <TopRatedMovies />
     </div>
   );
 };
---
+++ src/app/_components/Movielist/MoviesMaps.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState } from "react";
+import MovieCategory from "./MovieCategory";
+import { dataUpcomingList } from "@/app/_data/movie-list-data";
+import { dataPopularList } from "@/app/_data/movie-list-data";
+import { dataTopRatedList } from "@/app/_data/movie-list-data";
+import Movies from "./Movies";
+
+export const UpComingMovies = () => {
+  const [upComingMovies, setUpComingMovies] = useState(dataUpcomingList);
+  return (
+    <div className="flex flex-col gap-8">
+      <MovieCategory categoryName="Upcoming" />
+      <div className="grid grid-cols-2 gap-3 sm:gap-4 sm:grid-cols-3 md:gap-5 md:grid-cols-4 lg:gap-6 lg:grid-cols-5">
+        {upComingMovies.map(({ rate, img, id, title }) => (
+          <Movies key={id} img={img} title={title} rate={rate} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const PopularMovies = () => {
+  const [popularMovies, setPopularMovies] = useState(dataPopularList);
+  return (
+    <div className="flex flex-col gap-8">
+      <MovieCategory categoryName="Popular" />
+      <div className="grid grid-cols-2 gap-3 sm:gap-4 sm:grid-cols-3 md:gap-5 md:grid-cols-4 lg:gap-6 lg:grid-cols-5">
+        {popularMovies.map(({ rate, img, id, title }) => (
+          <Movies key={id} img={img} title={title} rate={rate} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const TopRatedMovies = () => {
+  const [topRatedMovies, setTopRatedMovies] = useState(dataTopRatedList);
+  return (
+    <div className="flex flex-col gap-8">
+      <MovieCategory categoryName="Top Rated" />
+      <div className="grid grid-cols-2 gap-3 sm:gap-4 sm:grid-cols-3 md:gap-5 md:grid-cols-4 lg:gap-6 lg:grid-cols-5">
+        {topRatedMovies.map(({ rate, img, id, title }) => (
+          <Movies key={id} img={img} title={title} rate={rate} />
+        ))}
+      </div>
+    </div>
+  );
+};
