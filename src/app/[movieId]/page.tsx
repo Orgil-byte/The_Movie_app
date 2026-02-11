550391390e0b14524d18c2dc7a7cf7a249ff36d3
Orgil-byte <ulziitogtohorgil@gmail.com>
2026-02-11 16:11:49 +0800

    A

--- src/app/[movieId]/page.tsx
+++ src/app/[movieId]/page.tsx
@@ -1,7 +1,7 @@
 import { MovieTrailer } from "../_components/TheMovieTrailer/MovieTrailer";
 import { MovieInfo } from "../_components/TheMovieTrailer/MovieInfo";
 import { MoreLikeThis } from "../_components/TheMovieTrailer/MoreLikeThis";
-import { getDirectorsActors, getPopularMovieById } from "@/lib/api";
+import { getDirectorsActors, getPopularMovieById , getMovieTrailers} from "@/lib/api";
 
 type MoviePagePropsType = {
   params: Promise<{ movieId: string }>;
@@ -11,10 +11,11 @@ const TheMovie = async ({ params }: MoviePagePropsType) => {
   const { movieId } = await params;
   const castCrew = await getDirectorsActors(movieId);
   const movie = await getPopularMovieById(movieId);
+  const trailers = await getMovieTrailers(movieId);
   return (
     <div className="pt-8 w-full flex flex-col gap-8 items-center">
       <div className="flex flex-col gap-8 w-full max-w-270">
-        <MovieTrailer movie={movie} castCrew={castCrew} />
+        <MovieTrailer movie={movie}  trailers={trailers}/>
         <MovieInfo movie={movie} castCrew={castCrew} />
         <MoreLikeThis />
       </div>
