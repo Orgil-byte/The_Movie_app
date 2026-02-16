8ee4c8bee72fa9d1424d81e399a41e778a7f4779
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-02-16 12:16:00 +0800

    A

--- src/app/Popular/page.tsx
+++ src/app/Popular/page.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/pagination";
 
 const PopularPage = async () => {
-  const data = await getPopularMovies();
+  const data = await getPopularMovies(3);
   return (
     <div className="w-full flex flex-col items-center my-10">
       <div className="px-5 flex flex-col w-full min-w-93.75 gap-8 md:px-10 lg:px-20 lg:gap-13 max-w-360">
@@ -24,24 +24,13 @@ const PopularPage = async () => {
                 href="#"
               />
             </PaginationItem>
-            <PaginationItem>
-              <PaginationLink className="dark:text-white" href="#">
-                1
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink className="dark:text-white" href="#" isActive>
-                2
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink className="dark:text-white" href="#">
-                3
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationEllipsis className="dark:text-white" />
-            </PaginationItem>
+            {[1, 2, 3].map((page) => {
+              <PaginationItem>
+                <PaginationLink className="dark:text-white" href="#">
+                  {page}
+                </PaginationLink>
+              </PaginationItem>;
+            })}
             <PaginationItem>
               <PaginationNext
                 className="dark:text-white dark:bg-neutral-900"
