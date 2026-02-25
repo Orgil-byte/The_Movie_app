38cd7a32540747626a1c29a8856fc8239d4c502b
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-02-25 10:59:51 +0800

    A

--- src/app/_components/HeroCarousel/HeroCarousel.tsx
+++ src/app/_components/HeroCarousel/HeroCarousel.tsx
@@ -5,19 +5,15 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import NameReviewStatus from "./NameReviewStatus";
-import WatchLaterBtn from "./WatchLaterBtn";
 import { Movie, Trailers } from "@/lib/movie-data-types";
 import { getMovieTrailers } from "@/lib/api";
-import Link from "next/link";
+import HeroSlide from "./HeroSlide";
 
 type HeroCarouselProps = {
   movies: Movie[];
 };
 
 const HeroCarousel = async ({ movies }: HeroCarouselProps) => {
-  const basImgurl = "https://image.tmdb.org/t/p/original";
-
   const theMovies = movies.slice(0, 3);
   const trailersPerMovie: Trailers[] = await Promise.all(
     theMovies.map((movie) => getMovieTrailers(movie.id.toString())),
@@ -26,33 +22,15 @@ const HeroCarousel = async ({ movies }: HeroCarouselProps) => {
   return (
     <div className="min-93.75 w-full overflow-hidden mb-12 relative">
       <Carousel className="min-w-93.75 w-full">
-        <CarouselNext className="hidden absolute w-10 h-10 right-[3%] z-30 cursor-pointer lg:flex"></CarouselNext>
+        <CarouselNext className="hidden absolute w-10 h-10 right-[3%] z-30 cursor-pointer lg:flex" />
         <CarouselContent>
           {theMovies.map((movie, index) => (
             <CarouselItem key={movie.id}>
-              <Link href={`/${movie.id}`} key={movie.id}>
-                <img
-                  className={`h-full w-full max-h-61.5 object-cover overflow-hidden sm:max-h-100 md:max-h-125 lg:max-h-150 xl:max-h-200 rounded-[5px]`}
-                  src={basImgurl + movie.backdrop_path}
-                  alt="Movie Img"
-                />
-
-                <div className="p-5 w-full mb-4 flex flex-col gap-4 md:p-10 md:gap-7 min-h-72 md:mb-8 lg:absolute lg:ml-25 lg:text-white lg:gap-2 lg:w-fit lg:top-[20%] xl:top-[30%]">
-                  <NameReviewStatus
-                    name={movie.title}
-                    rate={movie.vote_average}
-                  />
-                  <p className="text-sm font-normal text-[14px] leading-5 text-[#09090B] md:text-xl md:leading-7 lg:text-white lg:text-[12px] lg:leading-normal lg:w-75.5 dark:text-[#fafafa]">
-                    {movie.overview}
-                  </p>
-
-                  <WatchLaterBtn trailers={trailersPerMovie[index]} />
-                </div>
-              </Link>
+              <HeroSlide movie={movie} trailers={trailersPerMovie[index]} />
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious className="hidden absolute w-10 h-10 left-[3%] z-40 cursor-pointer lg:flex"></CarouselPrevious>
+        <CarouselPrevious className="hidden absolute w-10 h-10 left-[3%] z-40 cursor-pointer lg:flex" />
       </Carousel>
     </div>
   );
---
+++ src/app/_components/HeroCarousel/HeroSlide.tsx
@@ -0,0 +1,35 @@
+"use client";
+import NameReviewStatus from "./NameReviewStatus";
+import WatchLaterBtn from "./WatchLaterBtn";
+import { Movie, Trailers } from "@/lib/movie-data-types";
+import Link from "next/link";
+
+type HeroSlideProps = {
+  movie: Movie;
+  trailers: Trailers;
+};
+
+const HeroSlide = ({ movie, trailers }: HeroSlideProps) => {
+  const basImgurl = "https://image.tmdb.org/t/p/original";
+
+  return (
+    <Link href={`/${movie.id}`}>
+      <img
+        className="h-full w-full max-h-61.5 object-cover overflow-hidden sm:max-h-100 md:max-h-125 lg:max-h-150 xl:max-h-200 rounded-[5px]"
+        src={basImgurl + movie.backdrop_path}
+        alt="Movie Img"
+      />
+      <div className="p-5 w-full mb-4 flex flex-col gap-4 md:p-10 md:gap-7 min-h-72 md:mb-8 lg:absolute lg:ml-25 lg:text-white lg:gap-2 lg:w-fit lg:top-[20%] xl:top-[30%]">
+        <NameReviewStatus name={movie.title} rate={movie.vote_average} />
+        <p className="text-sm font-normal text-[14px] leading-5 text-[#09090B] md:text-xl md:leading-7 lg:text-white lg:text-[12px] lg:leading-normal lg:w-75.5 dark:text-[#fafafa]">
+          {movie.overview}
+        </p>
+        <div onClick={(e) => e.stopPropagation()}>
+          <WatchLaterBtn trailers={trailers} />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default HeroSlide;
