72889389ba0774d4a7d1fdc802955f3e6a4d8649
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-02-26 15:15:07 +0800

    A

--- src/lib/api/option.ts
+++ src/lib/api/option.ts
@@ -1,4 +1,5 @@
-const token = process.env.TMDB_TOKEN;
+const token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Mzk5MzRjZmY0ZTZkYzYxYjM3OGYyZDI0NjQ1MDIyNiIsIm5iZiI6MTc3MDYwNzgwMy45MDgsInN1YiI6IjY5ODk1NGJiYjljNmUwNTAxMDhjOTQ0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ADU9lSBgWoG_Xg1CZAqAwxrYCvdJu7STsz4cxQu-PNI";
 
 export const options = {
   method: "GET",
